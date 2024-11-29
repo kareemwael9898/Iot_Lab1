6dcf2fe669ee9d005526e4d60267e8e8eb780610
kareemwael9898 <kareemwael9898@gmail.com>
2024-11-29 20:29:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const instance = express();
+const port = 4401;
+
+let storedData = [];
+
+instance.use(bodyParser.urlencoded({ extended: true }));
+
+const server = instance.listen(port, () => {
+  console.log('Server started on port', port);
+});
+
+instance.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+
+  // console.log(x);
+});
+
+instance.post('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  let formData = req.body;
+
+  storedData.push(formData);
+
+  // WebSocket send data to clients
+  clients(formData);
+
+  console.log(formData);
+});
+
+const ws = require("ws").Server;
+const websocket = new ws({ server });
+
+websocket.on("connection", function (wss) {
+  console.log("Connection success");
+
+  wss.send(JSON.stringify(storedData));
+
+  wss.on("message", function (msg) {
+    console.log("server message: ", msg.toString());
+
+    clients(msg.toString());
+  });
+});
+
+function clients(data) {
+  websocket.clients.forEach(element => {
+    if (element.readyState === element.OPEN) {
+      element.send(JSON.stringify(data));
+
+
+    } else {
+      console.log("item disconnected");
+    }
+  });
+
+}
